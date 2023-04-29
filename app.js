aab87311c9f9902577f19cf40fa9a6672ead2362
Annie Guo <anniewguo@Annies-MacBook-Air-2.local>
2023-04-28 23:48:07 -0400

    slide right implementation added

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ document.addEventListener("keyup", (e) => {
     }
 })
 
-function removeZeros(arr) {
+function removeZerosLeft(arr) {
     let nonZeros = []
     for (let i = 0; i < columns; i++) {
         if (arr[i] !== 0) {
@@ -101,7 +101,7 @@ function removeZeros(arr) {
 
 function slideLeft() {
     for (let r = 0; r < rows; r++) {
-        removeZeros(board[r]);
+        removeZerosLeft(board[r]);
         for (let c = 0; c < columns - 1; c++) {
             if (board[r][c] == board[r][c + 1]) {
                 board[r][c] *= 3;
@@ -109,7 +109,7 @@ function slideLeft() {
                 c++;
             }
         }
-        removeZeros(board[r]);
+        removeZerosLeft(board[r]);
         for (let c = 0; c < columns; c++) {
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             updateTile(tile, board[r][c]);
@@ -117,8 +117,42 @@ function slideLeft() {
     }
 }
 
-function slideRight() {
+function removeZerosRight(arr) {
+    let nonZeros = []
+    for (let i = 0; i < columns; i++) {
+        if (arr[i] !== 0) {
+            nonZeros.push(arr[i]);
+        }
+    }
+    let len = columns - nonZeros.length;
+    let index = 0;
+    for (let i = 0; i < columns; i++) {
+        if (i < len) {
+            arr[i] = 0;
+        } else {
+            arr[i] = nonZeros[index];
+            index++;
+        }
+    }
+    return arr;
+}
 
+function slideRight() {
+    for (let r = 0; r < rows; r++) {
+        removeZerosRight(board[r]);
+        for (let c = columns - 1; c > 0; c--) {
+            if (board[r][c] == board[r][c - 1]) {
+                board[r][c] *= 3;
+                board[r][c - 1] = 0;
+                c--;
+            }
+        }
+        removeZerosRight(board[r]);
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            updateTile(tile, board[r][c]);
+        }
+    }
 }
 
 function slideUp() {
